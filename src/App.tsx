4ca04a0a8c89fbf8a78b0cc9fc7fe62a666b1419
Robert King <robert.o.king@hotmail.co.uk>
2022-04-16 23:01:08 +0100

    feat: Add controlled <input> for search-box

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>???</h1>
         <h2>Bits and pieces ideas for cryptic crossword setters</h2>
       </header>
+      <main>
+        <input aria-label="search-box" value={searchTerm} onChange={handleChange} />
+      </main>
     </div>
   );
 }
